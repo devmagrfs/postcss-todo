0e5596973f2d83b070f7a61393e6ad1a2428111b
devmagrfs <devmagrfs@gmail.com>
2022-11-03 23:54:47 +0900

    [Feat] 할 일 목록 추가하기 기능

---
+++ src/components/AddTodo/AddTodo.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import styles from './AddTodo.module.css';
+
+function AddTodo({ setTodoList, handleAddTodo }) {
+  const [todo, setTodo] = useState('');
+
+  const handleInputChange = (event) => {
+    setTodo(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (todo.trim().length === 0) return;
+    handleAddTodo({ id: '..', name: todo, isCompleted: false });
+    setTodo('');
+  };
+
+  return (
+    <form>
+      <input
+        type='text'
+        name='name'
+        placeholder='할 일을 입력해 주세요.'
+        value={todo}
+        onChange={handleInputChange}
+      />
+      <button type='submit' onClick={handleSubmit}>추가</button>
+    </form>
+  )
+}
+
+export default AddTodo
