1de18b4f0de825665742371d411223f79e5d43cd
devmagrfs <devmagrfs@gmail.com>
2022-11-03 23:59:19 +0900

    [Feat] uuid를 이용한 고유 id 적용하기

--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
 import styles from './AddTodo.module.css';
 
 function AddTodo({ setTodoList, handleAddTodo }) {
@@ -11,7 +12,7 @@ function AddTodo({ setTodoList, handleAddTodo }) {
   const handleSubmit = (event) => {
     event.preventDefault();
     if (todo.trim().length === 0) return;
-    handleAddTodo({ id: '..', name: todo, isCompleted: false });
+    handleAddTodo({ id: uuidv4(), name: todo, isCompleted: false });
     setTodo('');
   };
 
