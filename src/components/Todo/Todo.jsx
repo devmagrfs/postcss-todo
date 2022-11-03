3fbac7218d4fb948062516da14be6983f1a921b1
devmagrfs <devmagrfs@gmail.com>
2022-11-04 02:36:30 +0900

    [Style] Todo 스타일링 및 html 태그 구조 변경

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -3,7 +3,7 @@ import { BsFillTrashFill } from 'react-icons/bs'
 import styles from './Todo.module.css';
 
 function Todo({ todo, onUpdateTodo, onDeleteTodo }) {
-  const { name, isCompleted } = todo;
+  const { id, name, isCompleted } = todo;
 
   const handleChange = (event) => {
     const isCompleted = event.target.checked ? true : false;
@@ -13,18 +13,22 @@ function Todo({ todo, onUpdateTodo, onDeleteTodo }) {
   const handleDelete = () => onDeleteTodo(todo);
 
   return (
-    <li>
-      <label className={styles.label}>
-        <input
-          type='checkbox'
-          checked={isCompleted === true}
-          onChange={handleChange}
-        />
+    <li className={styles.todo}>
+      <input
+        type='checkbox'
+        checked={isCompleted === true}
+        onChange={handleChange}
+        className={styles.checkbox}
+        id={id}
+      />
+      <label htmlFor={id} className={styles.text}>
         {name}
       </label>
-      <button onClick={handleDelete}>
-        <BsFillTrashFill />
-      </button>
+      <span className={styles.icon}>
+        <button onClick={handleDelete} className={styles.button}>
+          <BsFillTrashFill />
+        </button>
+      </span>
     </li>
   )
 }
